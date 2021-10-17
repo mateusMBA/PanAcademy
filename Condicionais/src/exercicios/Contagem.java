d4fae82a20a78a6f36cefeee4b57759bce8418c9
Mateus Almeida <mateus.barros.almeida@gmail.com>
2021-10-16 22:07:06 -0300

    adicionando exercicios aula 15/10

---
+++ Condicionais/src/exercicios/Contagem.java
@@ -0,0 +1,18 @@
+package exercicios;
+
+/*
+Faça um programa que mostre uma contagem na tela de 233 a 456, só que contando de 3 em 3 
+quando estiver entre 300 e 400 e de 5 em 5 quando não estiver.
+ */
+
+public class Contagem {
+
+	public static void main(String[] args) {
+		
+		for(int i = 233; i <= 456;) {
+			
+			System.out.println(i);
+			i+= (i >= 300 && i <= 400) ? 3 : 5;
+		}
+	}
+}
--- Condicionais/src/exercicios/MediaSalarioFilhos.java
+++ Condicionais/src/exercicios/MediaSalarioFilhos.java
@@ -1,5 +1,7 @@
 package exercicios;
 
+import java.util.Scanner;
+
 /*
 	1- A prefeitura de uma cidade fez uma pesquisa entre 20 de seus habitantes, coletando dados sobre o salário e número de filhos. A prefeitura deseja saber:  
 	a) média do salário da população;
@@ -12,6 +14,32 @@ public class MediaSalarioFilhos {
 
 	public static void main(String[] args) {
 		
+		int totalFilhos = 0, salarioAteCem = 0;
+		double salario, totalSalario = 0.0, maiorSalario = 0.0;
+		
+		Scanner sc = new Scanner (System.in);
+		
+		for(int i = 0; i < 20; i++) {
+			
+			System.out.println("Digite o seu salário");
+			salario = sc.nextDouble();
+			totalSalario += salario;
+			
+			if(salario > maiorSalario) {
+				maiorSalario = salario;
+			}
+			if(salario <= 100.0) {
+				salarioAteCem++;
+			}
+			
+			System.out.println("Digite quantos filhos e filhas você tem");
+			totalFilhos += sc.nextInt();		
+		}
+		
+		System.out.println("A média do salário da população é: " + totalSalario/20);
+		System.out.println("A média do número de filhos é: " + (double) totalFilhos/20);
+		System.out.println("O maior salário é: " + maiorSalario);
+		System.out.println("O percentual de pessoas com salário de até $100,00 é: " + salarioAteCem* 100 / 20 + "%");
 		
 	}
 }
